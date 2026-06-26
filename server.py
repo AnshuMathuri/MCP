ccf92019b80eeb7547ebd023a65c7269ef0db829
AnshuMathuri <anshumathuri964@gmail.com>
2026-06-26 14:28:44 +0530

    initial commit

---
+++ server.py
@@ -0,0 +1,21 @@
+"""
+step-1 : we need  to create MCP object
+similar to flask and FastAPI object
+app=Flask(__name__)===> app.run
+app=FastAPI()      ===> aap.run
+mcp=FastMCP()      ===> mcp.run
+"""
+
+from mcp.server.fastmcp import FastMCP
+mcp=FastMCP("whether server")
+
+"""Step-2: create the first tool"""
+
+@mcp.tool()
+def get_weather(city):
+    return(f'the weather in {city} is 35 degrees C')
+
+"""step-3: Run the server"""
+
+if __name__=="__main__":
+    mcp.run()
